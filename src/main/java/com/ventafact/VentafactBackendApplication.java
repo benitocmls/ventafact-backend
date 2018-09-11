33589c9eb0bc8a498a033d6b7e97cdf8ce9d62c3
root <danielcam.inf@gmail.com>
2018-09-10 21:43:57 -0500

    primera subida

---
+++ src/main/java/com/ventafact/VentafactBackendApplication.java
@@ -0,0 +1,12 @@
+package com.ventafact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentafactBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentafactBackendApplication.class, args);
+	}
+}
